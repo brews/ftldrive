c9452653cd7d17f965df71d25a80be514d8a4cef
Brewster Malevich <sbmalev@gmail.com>
2018-04-19 14:58:40 -0700

    Refactor backend tests into single test script for simplification.

--- ftldrive/tests/test_backend.py
+++ ftldrive/tests/test_backend.py
@@ -12,7 +12,7 @@ def simple_bstate():
     return bstate
 
 
-@pytest.mark.parametrize('in_kwargs,goal', [
+basic_test_conditions = [
     ({'obs_value': 1.35, 'obs_error': 0.05, 'obs_idx': 4},
      {'mean': np.array([0.90578947, 0.90578947, 0.90578947,
                         0.90578947, 1.40578947, 0.90578947,
@@ -44,10 +44,28 @@ def simple_bstate():
       'var': np.array([0.00757937, 0.00757937, 0.00757937,
                        0.00757937, 0.00757937, 0.00757937,
                        0.00757937, 0.00757937, 0.00757937])}),
+     ]
 
-])
-def test_sequential_ekf(simple_bstate, in_kwargs, goal):
+
+def assert_assimilation_moments(observed, desired):
+    """Assert-test the mean and variance of an observed state with dired mean and var"""
+    np.testing.assert_allclose(observed.mean(axis=1), desired['mean'], atol=1e-3, rtol=0)
+    np.testing.assert_allclose(observed.var(axis=1), desired['var'], atol=1e-3, rtol=0)
+
+
+@pytest.mark.parametrize('in_kwargs,goal', basic_test_conditions)
+def test_python_sequential_ekf(simple_bstate, in_kwargs, goal):
+    ustate = sequential_ekf(simple_bstate.copy(), backend='python', **in_kwargs)
+    assert_assimilation_moments(ustate, goal)
+
+
+@pytest.mark.parametrize('in_kwargs,goal', basic_test_conditions)
+def test_cython_sequential_ekf(simple_bstate, in_kwargs, goal):
     ustate = sequential_ekf(simple_bstate.copy(), backend='cython', **in_kwargs)
+    assert_assimilation_moments(ustate, goal)
 
-    np.testing.assert_allclose(ustate.mean(axis=1), goal['mean'], atol=1e-3, rtol=0)
-    np.testing.assert_allclose(ustate.var(axis=1), goal['var'], atol=1e-3, rtol=0)
+
+@pytest.mark.parametrize('in_kwargs,goal', basic_test_conditions)
+def test_numba_sequential_ekf(simple_bstate, in_kwargs, goal):
+    ustate = sequential_ekf(simple_bstate.copy(), backend='numba', **in_kwargs)
+    assert_assimilation_moments(ustate, goal)
