Closes #1 - correcting use of background covariance inflation.
The correction is based on methods in Whitaker et al. 2003.

ftldrive/backend/numba/ensrf.py
@@ -19,9 +19,10 @@ def serial_ensrf(state, obs_value, obs_error, obs_idx, inflation=None,
         1D (p) array containing indexes indicating which state vector element
         corresponds to a given observation.
     inflation : array_like
-        1D (m) array of values used to inflate the background error covariance
-        matrix. Values of 1 will leave the covariance unchanged. This inflation
-        is applied once, before any observations are assimilated.
+        1D (m) inflation factor applied to background state deviations before
+        the background error covariance is calculated and before any
+        observations are assimilated. A value of 1 leaves the covariance
+        unchanged.
     localization : array_like
         2D (p x m) or 1D (p) array of values multiplied against the background error
         covariance matrix to limit the influence of each observation on the
@@ -96,9 +97,10 @@ def obs_assimilation_loop(state, obs, obs_error, obs_idx, inflation,
         1D (p) array containing indexes indicating which state vector element
         corresponds to a given observation.
     inflation : array_like
-        1D (m) array of values used to inflate the background error covariance
-        matrix. Values of 1 will leave the covariance unchanged. This inflation
-        is applied once, before any observations have been assimilated.
+        1D (m) inflation factor applied to background state deviations before
+        the background error covariance is calculated and before any
+        observations are assimilated. A value of 1 leaves the covariance
+        unchanged.
     localization : array_like
         2D (p x m) or 1D (p) array of values multiplied against the background
         error covariance matrix to limit the influence of each observation on
@@ -111,16 +113,18 @@ def obs_assimilation_loop(state, obs, obs_error, obs_idx, inflation,
     """
     p = len(obs)
 
-    this_state = update(xb=state, yb=state[obs_idx[0]], y0=obs[0],
+    updated_state = update(xb=state, yb=state[obs_idx[0]], y0=obs[0],
                         r=obs_error[0], infl=inflation, loc=localization[0])
 
     if p == 1:
-        return this_state
+        return updated_state
 
     for i in range(1, p):
-        this_state[...] = update(xb=this_state, yb=this_state[obs_idx[i]],
-                                 y0=obs[i], r=obs_error[i], loc=localization[i])
-    return this_state
+        updated_state[...] = update(xb=updated_state,
+                                    yb=updated_state[obs_idx[i]],
+                                    y0=obs[i], r=obs_error[i],
+                                    loc=localization[i])
+    return updated_state
 
 
 @njit
@@ -139,8 +143,10 @@ def update(xb, yb, y0, r, infl=1, loc=1):
     r : scalar
         Observation error (variance).
     infl : array_like
-        1D (m) for background error covariance inflation. A value of 1 leaves
-        the covariance unchanged.
+        1D (m) inflation factor applied to background state deviations before
+        the background error covariance is calculated and before any
+        observations are assimilated. A value of 1 leaves the covariance
+        unchanged.
     loc : array_like
         1D (m) covariance localization weights for background error covariance.
         A value of 1 leaves the covariance unchanged.
@@ -165,6 +171,9 @@ def update(xb, yb, y0, r, infl=1, loc=1):
     xb_bar = ensemble_mean(xb)  # (m x 1)
     xb_prime = xb - xb_bar  # (m x n)
 
+    # Apply background deviation inflation.
+    xb_prime *= infl
+
     # Obs estimate mean and deviation.
     yb_bar = np.mean(yb)  # (scalar)
     yb_prime = yb - yb_bar  # (m)
@@ -174,8 +183,7 @@ def update(xb, yb, y0, r, infl=1, loc=1):
     yb_prime_var = sample_var_1d(yb_prime, ddof=1)  # (scalar)
     xb_prime_yb_prime_cov = sample_cov_2d1d(xb_prime, yb_prime, ddof=1)  # (m)
 
-    # Apply covariance inflation and localization weights
-    xb_prime_yb_prime_cov *= infl
+    # Apply covariance  localization weights.
     xb_prime_yb_prime_cov *= loc
 
     # Assemble kalman gains

ftldrive/tests/test_numba_ensrf.py
@@ -169,11 +169,11 @@ def test_serial_ensrf_inflation():
     obs_error = np.array([0.25, 0.35])
     obs_idx = np.array([(2), (0)])
     inflation = np.ones(m) * 2.0
-    goal = np.array([[0.16165305, 0.56363636, 0.96561968],
-                     [3.16165305, 3.56363636, 3.96561968],
-                     [6.16165305, 6.56363636, 6.96561968],
-                     [9.16165305, 9.56363636, 9.96561968],
-                     [12.16165305, 12.56363636, 12.96561968]])
+    goal = np.array([[-0.48309776, 0.51431828, 1.51173433],
+                     [2.51690224, 3.51431828, 4.51173433],
+                     [5.51690224, 6.51431828, 7.51173433],
+                     [8.51690224, 9.51431828, 10.51173433],
+                     [11.51690224, 12.51431828, 13.51173433]])
     actual = serial_ensrf(state=state, obs_value=obs, obs_error=obs_error,
                           obs_idx=obs_idx, inflation=inflation, localization=None)
     np.testing.assert_allclose(actual, goal, atol=1e-7, rtol=0)
@@ -206,13 +206,12 @@ def test_serial_ensrf_localization_inflation():
     inflation = np.ones(m) * 2.0
     localization = np.array([[0, 0.5, 1.0, 0.5, 0],
                              [1.0, 0.5, 0, 0, 0]])
-    goal = np.array([[0.02319024, 0.77777778, 1.53236532],
-                     [3.08478351, 3.71959924, 4.35441498],
-                     [6.1527864, 6.6, 7.0472136],
-                     [9.0763932, 9.8, 10.5236068],
-                     [12.0, 13.0, 14.0]])
+    goal = np.array([[-0.55951638, 0.72413793, 2.00779225],
+                     [2.26619834, 3.68113057, 5.09606279],
+                     [5.1527864, 6.6, 8.0472136],
+                     [8.0763932, 9.8, 11.5236068],
+                     [11.0, 13.0, 15.0]])
     actual = serial_ensrf(state=state, obs_value=obs, obs_error=obs_error,
                           obs_idx=obs_idx, inflation=inflation,
                           localization=localization)
     np.testing.assert_allclose(actual, goal, atol=1e-7, rtol=0)
-